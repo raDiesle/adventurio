fcf063f9d156da4d13515db5c12b9708810f6b77
David Amend <david.amend@it-amend.de>
2012-02-01 22:34:34 +0100

    try to mock ajax

--- _attachments/src_tests/adventurio/models/LoginTest.js
+++ _attachments/src_tests/adventurio/models/LoginTest.js
@@ -1,7 +1,32 @@
-
 describe("Example", function() {
-	it("should expose an attribute", function() {
-		expect("hi").toEqual("What");
-		
+
+	var onSuccess, request;
+
+	beforeEach(function() {
+		jasmine.Ajax.useMock();
+		onSuccess = jasmine.createSpy('onSuccess');
+	});
+	it("should Login user", function() {
+
+		// onSuccess = jasmine.createSpy('onSuccess');
+
+		expect(adventurio.models.UserSingleton.isAuthenticated()).toBe(false);
+		// adventurio.models.UserSingleton.login("testuser", "mypasswd");
+
+		var response = ({
+			"ok" : true,
+			"name" : "simpleUser",
+			"roles" : []
+		});
+		adventurio.models.UserSingleton.checkLogin(response);
+
+		request = mostRecentAjaxRequest();
+
+		request.response = '{"ok" : true,"userCtx" : {"name" : "simpleUser","roles" : []},"info" : {"authentication_db" : "_users","authentication_handlers" : [ "oauth", "cookie", "default" ],"authenticated" : "cookie"}}';
+
+		expect(onSuccess).toHaveBeenCalled();
+
+		expect(adventurio.models.UserSingleton.isAuthenticated()).toBe(true);
+
 	});
 });
