8c8fbaad097f369745693271ee1d17560cacc59d
David Amend <david.amend@it-amend.de>
2012-06-04 22:16:20 +0200

    Start refactor move button + save field options

--- _attachments/src/adventurio/views/creator/EditPageFormItemOptionsWindow.js
+++ _attachments/src/adventurio/views/creator/EditPageFormItemOptionsWindow.js
@@ -13,10 +13,22 @@ adventurio.views.creator.PageElementEditor = adventurio.views.superClasses.Basic
 			hPos : this.parameter.hPos,
 			fieldPos : this.parameter.fieldPos,
 			storyId : this.model.id,
-			field : this.model.getModelFieldPath(this.options.parameter.vPos, this.options.parameter.hPos, this.options.parameter.fieldPos)
+			field : this.getCurrentField()
 		}
 	},
+	initialize : function() {
+		this.model.on('change', this.render, this);
+		this.model.lazyFetch();
+	},
+	getCurrentField : function(){
+		return this.model.getModelFieldPath(this.options.parameter.vPos, this.options.parameter.hPos, this.options.parameter.fieldPos);
+	},
 	onSuccessfulValidation : function(){
-		alert("saved");	
+		var fieldOptionValues = $("form", this.el).serializeJSON();
+		
+		this.getCurrentField().value = fieldOptionValues.value.replace(/^\s+|\s+$/g, "").replace(/\n/g, "<br>");
+		// .replace( /\r?\n/g, "\r\n"
+		// this.model.trigger("change");
+		this.model.save();
 	}
 }); 
--- _attachments/src/adventurio/views/creator/ManagePage.js
+++ _attachments/src/adventurio/views/creator/ManagePage.js
@@ -18,48 +18,5 @@ adventurio.views.creator.ManagePage = adventurio.views.superClasses.Basic.extend
 	initialize : function() {
 		this.model.on('change', this.render, this);
 		this.model.lazyFetch();
-	},
-	saveEditedValue : function(clickEvent) {
-		clickEvent.preventDefault();
-		this.attributes.editModeStatus.LEAVES_WRITE_MODE = true;
-		this.attributes.editModeStatus.READ_MODE = true;
-
-		var fieldPos = $(clickEvent.currentTarget).data("identity");
-		var vPos = this.options.parameter.vPos;
-		var hPos = this.options.parameter.hPos;
-
-		// var fieldToChange = this.model.getModelFieldPath(vPos, hPos, fieldPos);
-		var editedValueToSave = $(clickEvent.currentTarget).prev().val();
-		var fieldToBeChanged = this.model.getModelFieldPath(vPos, hPos, fieldPos);
-		fieldToBeChanged.value = editedValueToSave.replace(/^\s+|\s+$/g, "").replace(/\n/g, "<br>");
-		// .replace( /\r?\n/g, "\r\n"
-		this.model.save();
-		// this.model.toJSON()
-		// this.model.set({fields : });
-		// this.model.setModelFieldValue(vPos, hPos, fieldPos, editedValueToSave);
-
-	},
-	triggerCreate : function(event) {
-		event.stopPropagation();
-		var containerEditElement = $(event.currentTarget);
-
-		// && !this.attributes.editModeStatus.LEAVES_WRITE_MODE
-		if(this.attributes.editModeStatus.READ_MODE && !this.attributes.editModeStatus.LEAVES_WRITE_MODE) {
-			var context = {};
-			context.value = containerEditElement.html().replace(/<br>/g, "\n");
-			context.pos = containerEditElement.data("identity");
-
-			containerEditElement.html(adventurio.templates.creator.page.fields.StaticText.edit.compile(context));
-
-			// hack to support autoscroll
-			// $('.edit_area').trigger('create');
-			// $('input').textinput();
-			this.attributes.editModeStatus.READ_MODE = false;
-		} else if(this.attributes.editModeStatus.LEAVES_WRITE_MODE) {
-			// go to read mode
-			// var valueForReadMode = $('textarea', containerEditElement).val().replace(/\n/g, "<br>");
-			// containerEditElement.html(valueForReadMode);
-			this.attributes.editModeStatus.LEAVES_WRITE_MODE = false;
-		}
 	}
 });
